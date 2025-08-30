fc6d55aac8aa2843b11cc65b61ae6105a25465a9
Danburen <2964361672@qq.com>
2025-08-31 00:16:16 +0800

    chore: add styles & add app navigations & locales

--- waterfun-admin/src/App.vue
+++ waterfun-admin/src/App.vue
@@ -1,47 +1,232 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script setup lang="ts">
+import {
+  Edit,House, VideoCamera , ArrowRight
+} from '@element-plus/icons-vue'
+import MainNavBar from "@/components/MainNavBar.vue";
+import { ref, onMounted } from 'vue'
+import type {TagItem} from "@/types/TagItemsType";
+import TagNavigation from "@/components/TagNavigation.vue";
+
+const menuCollapse = ref(false)
+const tagNavContainer = ref(null)
+
+const handleWheel = (e) => {
+  if(! tagNavContainer.value) return
+  const scroll = tagNavContainer.value
+  console.log(scroll.scrollLeft,e.deltaY)
+  scroll.scrollLeft = scroll.scrollLeft + e.deltaY
+}
+
+const tagList = ref<TagItem[]>([
+  {
+    name: 'HomePage',
+    locale: 'nav.home',
+    to: '/',
+    closeable: false
+  },
+  {
+    name: 'PermissionManager',
+    locale: 'nav.permissionManager',
+    to: '/permissionManager',
+    closeable: true
+  },
+  {
+    name: 'OnlineUser',
+    locale: 'nav.onlineUser',
+    to: '/onlineUser',
+    closeable: true
+  },
+  {
+    name: 'OnlineUser',
+    locale: 'nav.onlineUser',
+    to: '/onlineUser',
+    closeable: true
+  },
+  {
+    name: 'OnlineUser',
+    locale: 'nav.onlineUser',
+    to: '/onlineUser',
+    closeable: true
+  },
+  {
+    name: 'OnlineUser',
+    locale: 'nav.onlineUser',
+    to: '/onlineUser',
+    closeable: true
+  },
+  {
+    name: 'OnlineUser',
+    locale: 'nav.onlineUser',
+    to: '/onlineUser',
+    closeable: true
+  },
+  {
+    name: 'OnlineUser',
+    locale: 'nav.onlineUser',
+    to: '/onlineUser',
+    closeable: true
+  },
+  {
+    name: 'OnlineUser',
+    locale: 'nav.onlineUser',
+    to: '/onlineUser',
+    closeable: true
+  },
+  {
+    name: 'OnlineUser',
+    locale: 'nav.onlineUser',
+    to: '/onlineUser',
+    closeable: true
+  },
+  {
+    name: 'OnlineUser',
+    locale: 'nav.onlineUser',
+    to: '/onlineUser',
+    closeable: true
+  },
+  {
+    name: 'OnlineUser',
+    locale: 'nav.onlineUser',
+    to: '/onlineUser',
+    closeable: true
+  },
+  {
+    name: 'OnlineUser',
+    locale: 'nav.onlineUser',
+    to: '/onlineUser',
+    closeable: true
+  },
+  {
+    name: 'OnlineUser',
+    locale: 'nav.onlineUser',
+    to: '/onlineUser',
+    closeable: true
+  },
+  {
+    name: 'OnlineUser',
+    locale: 'nav.onlineUser',
+    to: '/onlineUser',
+    closeable: true
+  },
+  {
+    name: 'OnlineUser',
+    locale: 'nav.onlineUser',
+    to: '/onlineUser',
+    closeable: true
+  },
+  {
+    name: 'OnlineUser',
+    locale: 'nav.onlineUser',
+    to: '/onlineUser',
+    closeable: true
+  },
+  {
+    name: 'OnlineUser',
+    locale: 'nav.onlineUser',
+    to: '/onlineUser',
+    closeable: true
+  }
+])
+
+onMounted(() => {
+
+});
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div class="container full-page-container">
+    <!-- Global Header -->
+    <MainNavBar @collapse="menuCollapse = !menuCollapse" />
+    <!-- Main Container-->
+    <div class="main-container">
+        <el-menu class="aside-menu" default-active="1" :collapse="menuCollapse" >
+          <el-menu-item index="1">
+            <el-icon><House /></el-icon>
+            <template #title>
+              <span>{{ $t('nav.home') }}</span>
+            </template>
+          </el-menu-item>
+          <el-sub-menu index="2">
+            <template #title>
+              <el-icon class="menu-icon"><Edit /></el-icon>
+              <span>{{ $t('nav.manager') }}</span>
+            </template>
+            <el-menu-item><span>{{ $t('nav.userManager') }}</span></el-menu-item>
+            <el-menu-item><span>{{ $t('nav.roleManager') }}</span></el-menu-item>
+            <el-menu-item><span>{{ $t('nav.permissionManager') }}</span></el-menu-item>
+          </el-sub-menu>
+          <el-sub-menu index="3">
+            <template #title>
+              <el-icon class="menu-icon"><VideoCamera /></el-icon>
+              <span>{{ $t('nav.statusMonitor') }}</span>
+            </template>
+            <el-menu-item><span>{{ $t('nav.serverMonitor') }}</span></el-menu-item>
+            <el-menu-item><span>{{ $t('nav.dynamicWorld') }}</span></el-menu-item>
+            <el-menu-item><span>{{ $t('nav.onlineUser') }}</span></el-menu-item>
+          </el-sub-menu>
+        </el-menu>
+      <div class="content-container">
+        <div class="content-header default-border-bottom"> <TagNavigation v-model="tagList" /></div>
+        <div class="content-main">Main body</div>
+        <div class="content-footer default-border-top">
+          <div class="copyright">CopyRight © WaterFun 2025</div>
+        </div>
+      </div>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+.container {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh
+}
+.main-container {
+  display: flex;
+  flex-grow: 1;
+  min-height: 0;
+  min-width: 0;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.content-container {
+  background: white;
+  display: flex;
+  flex: 1;
+  min-height: 0;
+  min-width: 0;
+  flex-direction: column;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.content-main {
+  flex: 1; /* 占据剩余空间 */
+  padding: 20px;
+  overflow-y: auto; /* 内容过多时滚动 */
 }
+
+.content-footer {
+  max-height: 30px;
+  font-size: 12px;
+}
+
+.copyright {
+  float: right;
+  padding: 0.5em 1em;
+}
+
+.menu-icon{
+  width: 1em;
+  height: 1em;
+  margin-right: 8px
+}
+
+.aside-menu {
+  min-height: 100%;
+}
+
+.content-header {
+  padding: 0 1em;
+}
+
 </style>
---
+++ waterfun-admin/src/i18n.ts
@@ -0,0 +1,14 @@
+import {createI18n} from "vue-i18n";
+import zh from '../i18n/locales/zh_CN.json'
+import en from '../i18n/locales/en_US.json'
+const i18n = createI18n({
+    legacy: false,
+    locale: 'zh',
+    fallbackLocale: 'en',
+    messages: {
+        zh,
+        en
+    }
+})
+
+export default i18n;
